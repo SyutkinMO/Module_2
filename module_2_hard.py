d258dedfb2d27248d6e2a83ec889cbe074a6fe32
Maxim <syutkin.mo@gmail.com>
2024-09-15 15:26:45 +0500

    module_2_hard_complete

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,9 @@
 input_key = int(input('Введите число '))
 passord = ''
-
 for i in range(1, input_key):
     for j in range(i + 1, input_key):
-        s = i + j
-        if input_key % s == 0:
+        if input_key % (i + j) == 0:
             passord = passord + str(i) + str(j)
         else:
             continue
-
 print(int(passord))
